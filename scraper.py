f96b97b89bb0258fd9be88705b460531adf6ad9f
Trevor Powers <tcpowers15@gmail.com>
2019-04-13 10:58:26 -0400

    trying to get github issues resolved with atom

---
+++ scraper.py
@@ -0,0 +1,23 @@
+import wikipedia
+import csv
+
+def get_sentence(page_name):
+    # TODO: I think I can use this function for both get_random_english
+    # get_random_dutch. In theory this should just take a link to a page...
+
+
+def get_random_dutch(pages):
+    # TODO:
+
+
+def get_random_english(pages):
+    # TODO:
+
+
+def main():
+    # TODO:
+    x = 5
+
+
+if __name__ == "__main__":
+    main()
