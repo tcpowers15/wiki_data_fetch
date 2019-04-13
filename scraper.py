fb70bade30d353c3f5321427436e9bd6ccc563f2
Trevor Powers <tcpowers15@gmail.com>
2019-04-13 12:11:47 -0400

    wikipedia module has not been mantained well, need to rebuild this using a new module

--- scraper.py
+++ scraper.py
@@ -12,7 +12,15 @@ def clean(text):
 
 def get_text(page_name):
     # fetch summary
-    text = wikipedia.page(page_name).summary()
+    # again trying again if exception is thrown
+    text = None
+    while text is None:
+        try:
+            text = wikipedia.page(page_name).summary
+        except:
+            pass
+
+    text = wikipedia.page(page_name).summary
 
     # clean text to meet requirements of school project data samples
     text = clean(text)
@@ -27,18 +35,31 @@ def get_random_dutch(pages):
 def get_random_english(num_pages):
     sentences = []
     # generate random pages
-    pages = [wikipedia.random(1) for i in range(num_pages)]
+    pages = []
+    # request a random page from wikipedia
+    # the package seems to throw a lot of busy
+    # exceptions so I am taking the try try again approach
+    for i in range(num_pages):
+        result = None
+        while result is None:
+            try:
+                # try to get page, and try again if error
+                result = wikipedia.random(1)
+
+            except:
+                pass
+        pages.append(result)
+
     for page in pages:
         # get text from each page
-        for text in get_text(page):
-            # append to sentences
-            sentences.append(text)
+        sentences.append(get_text(page))
     return sentences
 
 
 def main():
-    english_sentences = get_random_english(10)
+    english_sentences = get_random_english(1)
     print(english_sentences)
+    print("fuck")
 
 
 if __name__ == "__main__":
